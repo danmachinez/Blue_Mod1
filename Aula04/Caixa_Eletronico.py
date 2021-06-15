0957f094958ca39b7c8f96f1f1313f51aff177b4
Dan Machinez <machinezdanilo@gmail.com>
2021-06-15 20:53:49 -0300

    alteracao nos nomes

--- Aula04/Caixa_Eletronico.py
+++ Aula04/Caixa_Eletronico.py
@@ -2,7 +2,7 @@
 # n2 = valor de 5
 # n3 = valor de 10
 # n4 = valor de 50
-#n5 = valor de 100
+# n5 = valor de 100
 
 quantidade_n1 = 0
 quantidade_n2 = 0
@@ -16,13 +16,14 @@ valor_n3 = 10
 valor_n4 = 50
 valor_n5 = 100
 
+Valor_minimo = 10
+Valor_maximo = 600
 
 
+quantidade_Sacar  = float(input(f'Qual a quantidade que vc gostaria de sacar? (Valores entre R$ {Valor_minimo} - R$ {Valor_maximo}) '))
 
-quantidade_Sacar  = float(input('Qual a quantidade que vc gostaria de sacar? (Valores entre R$ 10 - R$600) '))
-
-while quantidade_Sacar < 10 or quantidade_Sacar > 600:
-    quantidade_Sacar = float(input('A quantidade informada não é permitida. Favor informar um valor entre R$ 10 e R$600: '))
+while quantidade_Sacar < Valor_minimo or quantidade_Sacar > Valor_maximo:
+    quantidade_Sacar = float(input(f'A quantidade informada não é permitida. Favor informar um valor entre R$ {Valor_minimo} e R$ {Valor_maximo}: '))
 
 quantidade_Auxiliar = quantidade_Sacar
 
---
+++ Aula06/Somando.py
@@ -0,0 +1,10 @@
+list = []
+perguntas = 0
+
+n = int(input('Quantos números gostaria de inserir para a soma?\n'))
+
+while perguntas != n:
+    print('Digite um número pra soma: ')
+    perguntas += 1
+    if perguntas == n:
+    #incompleto
