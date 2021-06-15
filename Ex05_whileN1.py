ae42ec0751c013f2f62546e4f7ff3f2e84383bf1
Dan Machinez <machinezdanilo@gmail.com>
2021-06-15 17:22:07 -0300

    erased

--- Ex05_whileN1.py
+++
@@ -1,4 +0,0 @@
-sexo = input('Diga seu sexo biológico: [F/M]\n').upper()
-while sexo != 'F' and sexo !='M':
-    sexo = input('Entrada inválida! Diga seu sexo biológico, use F/M: ').upper()
-print('Obrigado!')
