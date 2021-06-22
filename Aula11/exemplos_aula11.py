ae8d4273969d02f50c45a29fe2f1ff9ef91350b4
Dan Machinez <machinezdanilo@gmail.com>
2021-06-22 20:24:59 -0300

    .

---
+++ Aula11/exemplos_aula11.py
@@ -0,0 +1,6 @@
+contatos_lista = [('Danilo', '999-2321'), ('Lorena', '9828-2918'), ('Ronaldo', '3213-3123'), ('Rosangela', '8382-3232')]
+
+contatos = dict(contatos_lista)
+
+print(contatos)
+print(contatos['Danilo'])
