93b2e2c4fd1eec6d50582bed41e1c84c7b7ee7bf
Dan Machinez <machinezdanilo@gmail.com>
2021-06-22 23:44:57 -0300

    adicionei garantia contra usuario e atualizei alguns prints

--- Aula12/cadastroPessoas.py
+++ Aula12/cadastroPessoas.py
@@ -5,11 +5,13 @@ listaMulheres = list()
 listaIdadesAcimaDaMedia = list()
 while resp == 'S':
     dicAtual = {}
-    nome = input('Digite seu nome:\n ').title()
-    genero = input('Digite seu gênero: [F/M]\n ').upper()
+    nome = input('Digite seu nome:\n').title()
+    genero = input('Digite seu gênero: [F/M]\n').upper()
+    while genero not in 'FM':
+        genero = input('DADO INVÁLIDO! Digite seu genêro: [F/M]\n').upper()
     if genero == 'F':
         listaMulheres.append(nome)
-    idade = int(input('Digite sua idade:\n '))
+    idade = int(input('Digite sua idade:\n'))
     listaIdade.append(idade)
     dicAtual['Nome'] = nome
     dicAtual['Gênero'] = genero
@@ -20,14 +22,15 @@ while resp == 'S':
         listaIdadesAcimaDaMedia.append(idade)
     resp = input('Deseja cadastrar mais alguém? [S/N]\n').upper()
     print()
-    if resp == 'N':
-        break
+    while resp not in 'SN':
+        resp = input('RESPOSTA INVÁLIDA! Deseja cadastrar mais alguém? [S/N]\n').upper()
+
 print('=-' * 30)
 print(f'A quantidade de pessoas cadastradas foram {len(listaFinal)}')
 print()
 print(f'A média de idade dos cadastrados é de {media} anos')
 print()
-print(listaMulheres)
+print(f'As mulheres cadastradas foram: {listaMulheres}')
 print()
-print(listaIdadesAcimaDaMedia)
+print(f'As idades acima da média foram: {listaIdadesAcimaDaMedia}')
 print('=-' * 30)
