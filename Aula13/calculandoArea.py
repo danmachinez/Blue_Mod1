e36e6b635af26b8336488bb9de29a61955bbf52e
Dan Machinez <machinezdanilo@gmail.com>
2021-06-23 22:49:47 -0300

    .

---
+++ Aula13/calculandoArea.py
@@ -0,0 +1,7 @@
+def area(n1, n2):
+    calculo = n1 * n2
+    print(f'A área do terreno é de {calculo}m²')
+
+larg = float(input('Digite a largura do terreno: '))
+comprimento = float(input('Digite o comprimento do terreno: '))
+area(larg, comprimento)
